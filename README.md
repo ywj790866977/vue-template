d21d4ed8b2df191787f4c7f30072c54a6f1f7814
niunan <browncloudtwo@gmail.com>
2019-08-20 08:33:24 +0700

    first commit

--- README.md
+++ README.md
@@ -1,29 +1,36 @@
-# hmsc_pc
+#一个 vue-cli 的模板
+主要是添加了一些常用的功能 1. 添加了 eslint,
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Run your tests
+
 ```
 npm run test
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Element from 'element-ui'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+//引入axios
+Vue.use(VueAxios, axios)
+//引入elment
+Vue.use(Element)
+//设置ajax访问基础路径
+// Vue.http.options.root = 'http://localhost:3000/';
+// Vue.http.options.emulateJSON = true;   //ajax post 访问格式
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
